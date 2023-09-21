9e40f7fdfdaa34c6169933a885ea4439c8b9c5b2
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-09-21 09:56:16 +0200

    checkboxes/label animations

--- my-app/src/components/Todo/TodoInput.js
+++ my-app/src/components/Todo/TodoInput.js
@@ -1,17 +1,12 @@
-import classes from "./TodoInput.module.css";
+import classes from './TodoInput.module.css';
 
 const TodoInput = () => {
-  return (
-    <div className={classes["inputs-container"]}>
-      <label htmlFor="todo-input" className={classes.label}></label>
-      <input
-        type="text"
-        id="todo-input"
-        placeholder="Create a new todo..."
-        className={classes[`input`]}
-      />
-    </div>
-  );
+	return (
+		<div className={classes['input-container']}>
+			<label htmlFor="todo-input" className={classes.label}></label>
+			<input type="text" id="todo-input" placeholder="Create a new todo..." className={classes[`input`]} />
+		</div>
+	);
 };
 
 export default TodoInput;
