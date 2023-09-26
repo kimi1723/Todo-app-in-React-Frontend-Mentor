65314c3308af43f1ee2c6a2187cfe88e202c5ffd
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-09-26 14:06:17 +0200

    additional design

--- my-app/src/components/Layout/Main.js
+++ my-app/src/components/Layout/Main.js
@@ -1,11 +1,17 @@
 import classes from './Main.module.css';
 import TodoList from '../Todo/TodoList';
 
+import { useSelector } from 'react-redux';
+
 const Main = () => {
+	const todosAmount = useSelector(state => state.todos.length);
+	const areThereAnyTodos = todosAmount > 0 ? true : false;
+
 	return (
 		<main className={classes.main}>
-			<TodoList />
-			<p className={classes['dnd-p']}>Drag and drop to reoder list</p>
+			{areThereAnyTodos && <TodoList />}
+			{areThereAnyTodos && <p className={classes['info-text']}>Drag and drop to reoder list</p>}
+			{!areThereAnyTodos && <p className={classes['info-text']}>Add your first todo!</p>}
 		</main>
 	);
 };
